5868bc8d897bf014d1ca0649305f83e548843845
Kevin Andel Josy <kevinandeljosy11@gmail.com>
2024-10-10 22:33:15 +0530

    Created components for Home,Who,Works and Contact

--- three-d-portfolio/src/App.jsx
+++ three-d-portfolio/src/App.jsx
@@ -1,8 +1,17 @@
+import Contact from "./components/Contact"
+import Hero from "./components/Hero"
+import Who from "./components/Who"
+import Works from "./components/Works"
 
 function App() {
 
   return (
-    <></>
+    <div>
+      <Hero />
+      <Who />
+      <Works />
+      <Contact />
+    </div>
   )
 }
 
