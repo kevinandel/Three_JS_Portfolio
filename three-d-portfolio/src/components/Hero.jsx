63403ef7c97553bd10bae93cf90c74f4f4f3a2f4
Kevin Andel Josy <kevinandeljosy11@gmail.com>
2024-10-11 19:32:46 +0530

    Completed Hero section but still have to add 3D model

--- three-d-portfolio/src/components/Hero.jsx
+++ three-d-portfolio/src/components/Hero.jsx
@@ -1,13 +1,113 @@
 import React from "react";
 import styled from "styled-components";
+import Navbar from "./Navbar";
 
 const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+  width: 1400px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  flex: 2;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 20px;
+`;
+
+const Title = styled.h1`
+  font-size: 74px;
+`;
+
+const WhatWeDo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Line = styled.img`
+  height: 5px;
+`;
+
+const Subtitle = styled.h2`
+  color: #da4ea2;
+`;
+
+const Desc = styled.p`
+  font-size: 24px;
+  color: lightgray;
+`;
+
+const Button = styled.button`
+  background-color: #da4ea2;
+  color: white;
+  font-weight: 500;
+  width: 100px;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+const Right = styled.div`
+  flex: 3;
+  position: relative;
+`;
+
+const Img = styled.img`
+  width: 600px;
+  height: 500px;
+  object-fit: contain;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+  animation: animate 2s infinite ease alternate;
+
+  @keyframes animate {
+    to{
+        transform: translateY(20px);
+    }
+  }
 `;
 
 const Hero = () => {
-  return <Section>Hero</Section>;
+  return (
+    <Section>
+      <Navbar />
+      <Container>
+        <Left>
+          <Title>Think. Make. Solve.</Title>
+          <WhatWeDo>
+            <Line src="./images/line.png" />
+            <Subtitle>What we do</Subtitle>
+          </WhatWeDo>
+          <Desc>
+            we enjoy creating delightful, human centered digital experiences.
+          </Desc>
+          <Button>Learn More</Button>
+        </Left>
+        <Right>
+          {/* {3d model} */}
+          <Img src="./images/moon.png" />
+        </Right>
+      </Container>
+    </Section>
+  );
 };
 
 export default Hero;
