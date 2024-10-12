eb0bdaa40667835cb6f6bbd46f85b69372b0bdc4
Kevin Andel Josy <kevinandeljosy11@gmail.com>
2024-10-12 21:04:17 +0530

    Created the 3D cube and got some basics of three js

--- three-d-portfolio/src/App.jsx
+++ three-d-portfolio/src/App.jsx
@@ -1,5 +1,6 @@
 import Contact from "./components/Contact";
 import Hero from "./components/Hero";
+import Test from "./components/Test";
 import Who from "./components/Who";
 import Works from "./components/Works";
 import styled, { createGlobalStyle } from "styled-components";
@@ -35,6 +36,7 @@ function App() {
         <Who />
         <Works />
         <Contact />
+        <Test />
       </Container>
     </>
   );
--- three-d-portfolio/src/components/Who.jsx
+++ three-d-portfolio/src/components/Who.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import styled from "styled-components";
+import Cube from "./Cube";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 
 const Section = styled.div`
   height: 100vh;
@@ -66,7 +69,14 @@ const Who = () => {
   return (
     <Section>
       <Container>
-        <Left>{/* {3d model} */}</Left>
+        <Left>
+          <Canvas camera={{ fov: 25, position: [5, 5, 5] }}>
+            <OrbitControls enableZoom={false} autoRotate />
+            <ambientLight intensity={1} />
+            <directionalLight position={[3, 2, 1]} />
+            <Cube />
+          </Canvas>
+        </Left>
         <Right>
           <Title>Think outside the square space</Title>
           <WhatWeDo>
